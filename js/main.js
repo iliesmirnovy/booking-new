deb3600dca25abb853bde9dff6f341ad825d9fef
iliesmirnovy <ilie.smirnovy@gmail.com>
2022-03-14 23:22:56 +0300

    first homework done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function getRandomIntFromRange(minNumber, maxNumber) {
+    if (minNumber > maxNumber) {
+        return 'Ошибка. Минимальное значение больше максимального';
+    }
+    return Math.min(Math.max(Math.round(Math.random() * maxNumber), minNumber), maxNumber);
+}
+
+function checkStringLength(stringToCheck, maxStringLength) {
+    return stringToCheck.length <= maxStringLength;
+}
