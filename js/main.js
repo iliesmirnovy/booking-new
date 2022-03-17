71ccd235701ea173e4a6390ab67a2b65acfe9189
iliesmirnovy <ilie.smirnovy@gmail.com>
2022-03-18 01:32:38 +0300

    Выполнена вторая часть задания 7 модуля

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 import { bookingArray } from './data.js'
 import { cardsArray } from './data.js';
+import { pageInactive, pageActive } from './form.js'
 
 
 const mapCanvas = document.querySelector('#map-canvas');
 mapCanvas.appendChild(cardsArray[1]);
+
+pageInactive();
+pageActive();
